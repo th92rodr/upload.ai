4a806fa44f8a9341e397908fe011195ca0331e26
th92rodr <45070690+th92rodr@users.noreply.github.com>
2024-09-18 18:17:44 -0300

    Server: create upload-video route

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,15 @@ import cors from '@fastify/cors'
 import fastify from 'fastify'
 
 import { env } from './env'
+import { uploadVideoRoute } from './routes/upload-video'
 
 const app = fastify()
 
 app.register(cors, {
   origin: '*',
 })
+
+app.register(uploadVideoRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running')
