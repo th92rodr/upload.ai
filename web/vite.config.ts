812651eb12ef834ff3d4a20815ef69760465fff9
th92rodr <thiago.clrodrigues@gmail.com>
2024-09-21 18:37:50 -0300

    add and setup ffmpeg dependencies

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,13 @@
-import path from 'path'
+import path from 'node:path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
