6457bb0e36ef5bc0cc515a431d322fa5d937776e
th92rodr <45070690+th92rodr@users.noreply.github.com>
2024-09-20 23:15:54 -0300

    Server: update fastify app and routes to set zod type provider

--- server/src/routes/upload-video.ts
+++ server/src/routes/upload-video.ts
@@ -4,45 +4,49 @@ import path from 'node:path'
 import { pipeline } from 'node:stream'
 import { promisify } from 'node:util'
 import { fastifyMultipart } from '@fastify/multipart'
-import type { FastifyInstance } from 'fastify'
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 
 import { prisma } from '../lib/prisma'
 
 const pump = promisify(pipeline)
 
-export async function uploadVideoRoute(app: FastifyInstance) {
+export const uploadVideoRoute: FastifyPluginAsyncZod = async app => {
   app.register(fastifyMultipart, {
     limits: {
       fileSize: 1_048_576 * 25, //25mb
     },
   })
 
-  app.post('/videos', async (request, reply) => {
-    const data = await request.file()
+  app.route({
+    method: 'POST',
+    url: '/videos',
+    handler: async (request, reply) => {
+      const data = await request.file()
 
-    if (!data) {
-      return reply.status(400).send({ error: 'Missing file input.' })
-    }
+      if (!data) {
+        return reply.status(400).send({ error: 'Missing file input.' })
+      }
 
-    const extension = path.extname(data.filename)
+      const extension = path.extname(data.filename)
 
-    if (extension !== '.mp3') {
-      return reply.status(400).send({ error: 'Invalid file input type.' })
-    }
+      if (extension !== '.mp3') {
+        return reply.status(400).send({ error: 'Invalid file input type.' })
+      }
 
-    const fileBaseName = path.basename(data.filename, extension)
-    const fileUploadName = `${fileBaseName}-${randomUUID()}${extension}`
-    const uploadDestination = path.resolve(__dirname, '../../tmp', fileUploadName)
+      const fileBaseName = path.basename(data.filename, extension)
+      const fileUploadName = `${fileBaseName}-${randomUUID()}${extension}`
+      const uploadDestination = path.resolve(__dirname, '../../tmp', fileUploadName)
 
-    await pump(data.file, createWriteStream(uploadDestination))
+      await pump(data.file, createWriteStream(uploadDestination))
 
-    const video = await prisma.video.create({
-      data: {
-        name: data.filename,
-        path: uploadDestination,
-      },
-    })
+      const video = await prisma.video.create({
+        data: {
+          name: data.filename,
+          path: uploadDestination,
+        },
+      })
 
-    return { video }
+      return { video }
+    },
   })
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,23 @@
 import cors from '@fastify/cors'
 import fastify from 'fastify'
+import {
+  type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
 import { env } from './env'
 import { getAllPromptsRoute } from './routes/get-all-prompts'
 import { uploadVideoRoute } from './routes/upload-video'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(cors, {
   origin: '*',
 })
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
 app.register(uploadVideoRoute)
 app.register(getAllPromptsRoute)
